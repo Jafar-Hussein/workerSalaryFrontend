5141e5665328f564bd13ef80f58e7e01b4a14525
Jafar Hussein <Jafarhussein6@gmail.com>
2024-05-04 22:28:43 +0200

    fixade

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
+import jwtDecode from 'jwt-decode';
 import '../css/login.css';
 import { Alert, Button } from 'react-bootstrap';
 import axios from 'axios';
